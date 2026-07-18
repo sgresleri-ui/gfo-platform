80f64d2be0a7d5ac5b66f8e97575935edecec376
Stefano Gresleri <LA_TUA_EMAIL_GITHUB>
2026-07-18 10:26:33 +0200

    chore: remove duplicate source files

--- backend/src/budget/budget.module 2.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BudgetController } from './budget.controller';
-import { BudgetService } from './budget.service';
-
-@Module({
-  controllers: [BudgetController],
-  providers: [BudgetService],
-})
-export class BudgetModule {}
